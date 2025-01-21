a14fd5e38a849b3b3db0be898b66c5343c16b8ec
mateop11 <mateo.pillajo@udla.edu.ec>
2025-01-21 07:54:35 -0500

    Se realizo la creacion de la clase Pelicula, de igual forma se realizo la cracion de una carpeta llamada servicios, en la cual se inserto la Base deDatosServicio

---
+++ MovieApp_MateoPillajo/Models/Pelicula.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp_MateoPillajo.Models
+{
+    internal class Pelicula
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public string ActorPrincipal { get; set; }
+        public string Premios { get; set; }
+        public string SitioWeb { get; set; }
+        public string Mpillajo { get; set; }
+
+    }
+}
