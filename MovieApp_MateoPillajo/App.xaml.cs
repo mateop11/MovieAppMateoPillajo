aaf7b05dc8d9a8a07270cd0c803113e52de1b982
laboratorio <laboratorio@UPOAULA10315>
2025-01-21 07:36:48 -0500

    Agregar archivos de proyecto.

---
+++ MovieApp_MateoPillajo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieApp_MateoPillajo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MovieApp_MateoPillajo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MovieApp_MateoPillajo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
