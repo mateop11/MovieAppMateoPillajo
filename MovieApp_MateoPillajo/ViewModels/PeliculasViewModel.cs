a1de5dec68b67bf77954c7e9a7914cddb43c2304
mateop11 <mateo.pillajo@udla.edu.ec>
2025-01-21 07:58:42 -0500

    Se realizo la creacion del view model de pelicula

--- MovieApp_MateoPillajo/ViewModels/PeliculasViewModel.cs
+++ MovieApp_MateoPillajo/ViewModels/PeliculasViewModel.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Net.Http.Json;
 
-namespace MovieApp_MateoPillajo.ViewModels
+using MovieApp_MateoPillajo.Models;
+using MovieApp_MateoPillajo.Servicios;
+
+namespace MovieApp_MateoPillajo.ViewModels;
+
+public class PeliculasViewModel : BaseViewModel
 {
-    internal class PeliculasViewModel
+    private readonly BaseDeDatosServicio _baseDeDatosServicio;
+    private string _consultaBusqueda;
+    public string ConsultaBusqueda
     {
+        get => _consultaBusqueda;
+        set => SetProperty(ref _consultaBusqueda, value);
+    }
+
+    public ObservableCollection<Pelicula> Peliculas { get; } = new();
+
+    public Command BuscarPeliculaCommand { get; }
+    public Command LimpiarBusquedaCommand { get; }
+
+    public PeliculasViewModel(BaseDeDatosServicio baseDeDatosServicio)
+    {
+        _baseDeDatosServicio = baseDeDatosServicio;
+
+        BuscarPeliculaCommand = new Command(async () => await BuscarPeliculaAsync());
+        LimpiarBusquedaCommand = new Command(() => ConsultaBusqueda = string.Empty);
+
+        CargarPeliculas();
+    }
+
+    private async Task BuscarPeliculaAsync()
+    {
+        if (string.IsNullOrWhiteSpace(ConsultaBusqueda)) return;
+
+        using var cliente = new HttpClient();
+        var respuesta = await cliente.GetFromJsonAsync<List<Pelicula>>($"https://freetestapi.com/api/v1/movies?search={ConsultaBusqueda}");
+
+        if (respuesta != null && respuesta.Any())
+        {
+            var pelicula = respuesta.First();
+            pelicula.MPillajo = "Mateo Pillajo";
+
+            await _baseDeDatosServicio.AgregarPeliculaAsync(pelicula);
+            CargarPeliculas();
+        }
+        else
+        {
+            await App.Current.MainPage.DisplayAlert("Error", "No se encontró ninguna película.", "Aceptar");
+        }
+    }
+
+    private async void CargarPeliculas()
+    {
+        Peliculas.Clear();
+        var peliculas = await _baseDeDatosServicio.ObtenerPeliculasAsync();
+        foreach (var pelicula in peliculas)
+        {
+            Peliculas.Add(pelicula);
+        }
     }
 }
